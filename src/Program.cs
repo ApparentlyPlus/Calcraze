a9ac70e16ef6b2e48da12a242e07bb643b564beb
ApparentlyPlus <panoschatzikallias@gmail.com>
2026-05-07 17:04:27 +0300

    Experimental project restructuring

---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using System;
+
+namespace Calcraze;
+
+sealed class Program
+{
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args)
+	{
+		try{
+			
+			BuildAvaloniaApp()
+			.StartWithClassicDesktopLifetime(args);
+		}
+		catch(Exception e){
+			Console.WriteLine(e);
+		}
+	}
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
